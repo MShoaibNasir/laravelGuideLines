e02dcc58b87dea3a6be0900de582beb5bc8ee613
M Shoaib nasir <95880880+CodeWithShoaib@users.noreply.github.com>
2023-01-17 06:06:09 +0500

    Update example.php

--- example.php
+++ example.php
@@ -1,14 +1,3 @@
-&lt;?php
-&lt;div&gt;
-    &lt;!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca --&gt;
-    &lt;h1&gt;my name is {{$name}}  and your father name is {{$fatherName}} and the news is {{$news}} and the 
-    
-    description is :- {{$description}}
-    &lt;/h1&gt;
-    &lt;div&gt;
-        &lt;h1 {{$attributes-&gt;merge(['class'=&gt;'dclass'])}}&gt;
-hello
-        &lt;/h1&gt;
-    &lt;/div&gt;
-&lt;/div&gt;
-?&lt;
+node js cheeetsheet
+
+https://www.codewithharry.com/blogpost/mongodb-cheatsheet/
